ec493f6c51a63e3314501758c42fc3815eac9b9b
Girish Aligati <girish.aligati@verifone.com>
2020-09-10 12:38:56 +0530

    Test

--- Heap/Heap_Class.py
+++ Heap/Heap_Class.py
@@ -1,86 +1,89 @@
 class Heap(object):
     def __init__(self,*argv):
-        self.Array = []
+        self.array = []
         if argv :
             for i in argv:
                 if type(i) == int :
-                    self.Array.append(i)
+                    self.array.append(i)
                 else:
                     print("Heap accepts only numericals")
         else:
-            self.Array = list(input("Enter the input array in comma seperated type ex: 1,2,3,4").split(','))
-        # self.N = len(Arr)
+            self.array = list(input("Enter the input array in comma seperated type ex: 1,2,3,4").split(','))
+        # self.length = len(Arr)
     #refer to the Donbader website for difference betewen __repr__ and __str__
     def __repr__(self):
-        return '{self.__class__.__name__}({self.Array})'.format(self = self)
+        return '{self.__class__.__name__}({self.array})'.format(self = self)
 
     def max_heapify(self):
-        N = len(self.Array)
-        for i in reversed(range(N//2)): #range(N//2,-1,-1):
-            self._maxheapify(self.Array,i,N)
+        length = len(self.array)
+        for i in reversed(range(length//2)): #range(length//2,-1,-1):
+            self.__maxheapify(self.array,i,length)
 
     def min_heapify(self):
-        N = len(self.Array)
-        for i in reversed(range(N//2)):
-            self._minheapify(self.Array,i,N)
+        length = len(self.array)
+        for i in reversed(range(length//2)):
+            self.__minheapify(self.array,i,length)
          
-    def _minheapify(self,Array,i,N):
-        # N = len(Arr) - 1
+    def __minheapify(self,array,i,length):
+        # length = len(Arr) - 1
         left = 2 * i + 1 
         right = 2 * i + 2
-        if(left < N and Array[i] > Array[left]):
+        if(left < length and array[i] > array[left]):
             largest = left
         else:
             largest = i
-        if(right < N and Array[largest] > Array[right]):
+        if(right < length and array[largest] > array[right]):
             largest = right
         if(largest != i):
-            Array[i],Array[largest] = Array[largest],Array[i]
-            self._minheapify(Array,largest,N)
+            array[i],array[largest] = array[largest],array[i]
+            self.__minheapify(array,largest,length)
 
-    def _maxheapify(self,Array,i,N):
-        # N = len(Arr) - 1
+    def __maxheapify(self,array,i,length):
+        # length = len(Arr) - 1
         left = 2 * i + 1 
         right = 2 * i + 2
-        if(left < N and Array[i] < Array[left]):
+        if(left < length and array[i] < array[left]):
             largest = left
         else:
             largest = i
-        if(right < N and Array[largest] < Array[right]):
+        if(right < length and array[largest] < array[right]):
             largest = right
         if(largest != i):
-            Array[i],Array[largest] = Array[largest],Array[i]
-            self._maxheapify(Array,largest,N)
+            array[i],array[largest] = array[largest],array[i]
+            self.__maxheapify(array,largest,length)
 
     def heap_sort(self):
         self.max_heapify()
-        N = len(self.Array)
-        for i in reversed(range(N)): #range(length,1,-1):
-            self.Array[0],self.Array[i] = self.Array[i],self.Array[0]
-            # N = N-1
-            self._maxheapify(self.Array,0,i)
+        length = len(self.array)
+        for i in reversed(range(length)): #range(length,1,-1):
+            self.array[0],self.array[i] = self.array[i],self.array[0]
+            # length = length-1
+            self.__maxheapify(self.array,0,i)
     
     def rev_heap_sort(self):
         self.min_heapify()
-        N = len(self.Array)
-        for i in reversed(range(N)): #range(length,1,-1):
-            self.Array[0],self.Array[i] = self.Array[i],self.Array[0]
-            # N = N-1
-            self._minheapify(self.Array,0,i)
+        length = len(self.array)
+        for i in reversed(range(length)): #range(length,1,-1):
+            self.array[0],self.array[i] = self.array[i],self.array[0]
+            # length = length-1
+            self.__minheapify(self.array,0,i)
 
     def extract_maximum(self):
         self.max_heapify()
-        top = self.Array[0]
-        N = len(self.Array)
-        self.Array[0] = self.Array.pop() #self.Array[N-1] 
-        self._maxheapify(self.Array,0,N-2)
+        top = self.array[0]
+        length = len(self.array)
+        self.array[0] = self.array.pop() #self.array[length-1] 
+        self.__maxheapify(self.array,0,length-2)
         return top
     
     def extract_manimum(self):
         self.min_heapify()
-        top = self.Array[0]
-        N = len(self.Array)
-        self.Array[0] = self.Array.pop() #self.Array[N-1] 
-        self._minheapify(self.Array,0,N-2)
+        top = self.array[0]
+        length = len(self.array)
+        self.array[0] = self.array.pop() #self.array[length-1] 
+        self.__minheapify(self.array,0,length-2)
         return top
 
+heap = Heap(1,2,3,4,5)
+heap.max_heapify()
+print(heap.array)
